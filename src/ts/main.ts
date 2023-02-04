2c466c4db9c5f5530fb0cf56d1b3932c7662e3fa
Kseniia Ivanova <kseniia.ivanova@gmail.com>
2023-02-04 23:48:11 +0100

    changed create html

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -1,5 +1,6 @@
 import { IRepo } from "./models/IRepo";
 
+import { Image } from "./models/Image";
 import { getRepos } from "./service/getRepos";
 
 getRepos().then((repos) => {
@@ -25,30 +26,44 @@ function createHTML(repos: IRepo[]) {
       "projects"
     ) as HTMLDivElement;
 
+    let images: Image[] = [
+      {
+        src: "https://www.dropbox.com/s/n5tcli390gel661/dice.png?raw=1",
+      },
+
+      {
+        src: "https://www.dropbox.com/s/kab9iaga7lff94v/webshop.png?raw=1",
+      },
+
+      {
+        src: "https://www.dropbox.com/s/bjxml3bw12rx4jw/memory.png?raw=1",
+      },
+
+      {
+        src: "https://www.dropbox.com/s/4tcsnz35e6s2jgx/todo.png?raw=1",
+      },
+    ];
+
     let project: HTMLDivElement = document.createElement("div");
     project.classList.add("project");
 
+    let screenshot: HTMLImageElement = document.createElement("img");
+    screenshot.setAttribute("src", images[i].src);
+    screenshot.classList.add("screenshot");
+
     let projectDesc: HTMLParagraphElement = document.createElement("p");
     projectDesc.classList.add("desc");
-
-    /*   let projectImage: HTMLImageElement = document.createElement("img");
-    projectImage.classList.add("screenshot");
-    projectImage.setAttribute(
-      "src",
-      "https://www.dropbox.com/s/5avrkgqypp05oqb/54455454.jpg?raw=1"
-    ); */
 
     let projLink: HTMLAnchorElement = document.createElement("a");
     projLink.href = chosenRepos[i].html_url;
     projLink.target = "_blank";
 
     projectDesc.innerHTML = chosenRepos[i].description;
-    //bullProj.className = "item__proj";
-    //projLink.innerHTML = chosenRepos[i].name + " ";
+
     projLink.innerHTML = "GitHub";
     projLink.className = "link__proj";
-    /* 
-    project.appendChild(projectImage); */
+    project.appendChild(screenshot);
+
     project.appendChild(projectDesc);
     project.appendChild(projLink);
     projCont.appendChild(project);
--- src/ts/models/IRepo.ts
+++ src/ts/models/IRepo.ts
@@ -3,3 +3,8 @@ export interface IRepo {
   name: string;
   description: string;
 }
+
+export interface Image {
+  name: string;
+  src: string;
+}
